d35c6f263d614466c5ab87f8fcd3dc466c58adde
Bas van Dijk <bass_van_dijk@live.nl>
2015-01-13 02:12:14 +0100

    cmmit
    no comment

--- index.php
+++ index.php
@@ -3,19 +3,22 @@
 	<title>Hoofdpagina</title>
 
 	<?php // All css and Js links ?>
-	<link href="bootstrap/dist/css/bootstrap-theme.css" rel="stylesheet" type="text/css">
-	<link href="bootstrap/dist/css/bootstrap-theme.css.map" rel="stylesheet" type="text/css">
-	<link href="bootstrap/dist/css/bootstrap-theme.min.css" rel="stylesheet" type="text/css">
-	<link href="bootstrap/dist/css/bootstrap.css" rel="stylesheet" type="text/css">
-	<link href="bootstrap/dist/css/bootstrap.css.map" rel="stylesheet" type="text/css">
-	<link href="bootstrap/dist/css/bootstrap.min.css" rel="stylesheet" type="text/css">
+	<link href="bootstrap/css/bootstrap-theme.css" rel="stylesheet" type="text/css">
+	<link href="bootstrap/css/bootstrap-theme.css.map" rel="stylesheet" type="text/css">
+	<link href="bootstrap/css/bootstrap-theme.min.css" rel="stylesheet" type="text/css">
+	<link href="bootstrap/css/bootstrap.css" rel="stylesheet" type="text/css">
+	<link href="bootstrap/css/bootstrap.css.map" rel="stylesheet" type="text/css">
+	<link href="bootstrap/css/bootstrap.min.css" rel="stylesheet" type="text/css">
 
 	<link href="css/main.min.css" rel="stylesheet" type="text/css">
 	<link href="css/main.css" rel="stylesheet" type="text/css">
 
-	<script src="bootstrap/dist/js/bootstrap.js"></script>
-	<script src="bootstrap/dist/js/bootstrap.min.js"></script>
-	<script src="bootstrap/dist/js/npm.js"></script>
+	<script src="//code.jquery.com/jquery-1.11.2.min.js"></script>
+	<script src="//code.jquery.com/jquery-migrate-1.2.1.min.js"></script>
+
+	<script src="bootstrap/js/bootstrap.js"></script>
+	<script src="bootstrap/js/bootstrap.min.js"></script>
+	<script src="bootstrap/js/npm.js"></script>
 
 	<script src="js/main.js"></script>
 </head>
@@ -25,5 +28,6 @@
 		<div id="header-logo"></div>
 		<div id="header-rest"></div>
 	</div>
+	<?php require_once 'php/html_includes/banner.php'; ?>
 </body>
 </html>
---
+++ php/html_includes/banner.php
@@ -0,0 +1,50 @@
+<div id="banner">
+	<div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
+
+  
+
+  <!-- Wrapper for slides -->
+  <div class="carousel-inner" role="listbox">
+    <div class="item active">
+      <img src="media/slider/image1.jpg" alt="...">
+      <div class="carousel-caption">
+		Image 1
+      </div>
+    </div>
+    <div class="item">
+      <img src="media/slider/image2.jpg" alt="...">
+      <div class="carousel-caption">
+		Image 2
+      </div>
+    </div>
+    <div class="item">
+      <img src="media/slider/image3.jpg" alt="...">
+      <div class="carousel-caption">
+		Image 2
+      </div>
+    </div>
+    <div class="item">
+      <img src="media/slider/image4.jpg" alt="...">
+      <div class="carousel-caption">
+		Image 2
+      </div>
+    </div>
+    <div class="item">
+      <img src="media/slider/image5	.jpg" alt="...">
+      <div class="carousel-caption">
+		Image 2
+      </div>
+    </div>
+  </div>
+
+  <!-- Controls -->
+  <a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
+    <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
+    <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
+</div>
+</div>
