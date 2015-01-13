90b17da55ad52a62ae28d112f4a0659dcc0e3a6f
Bas van Dijk <bass_van_dijk@live.nl>
2015-01-13 01:34:11 +0100

    Edited the logo even more
    Index.php now shows a preview of how the logo will look like in the
    existing header. I must say it doesn't look bad.

---
+++ index.php
@@ -0,0 +1,29 @@
+<html>
+<head>
+	<title>Hoofdpagina</title>
+
+	<?php // All css and Js links ?>
+	<link href="bootstrap/dist/css/bootstrap-theme.css" rel="stylesheet" type="text/css">
+	<link href="bootstrap/dist/css/bootstrap-theme.css.map" rel="stylesheet" type="text/css">
+	<link href="bootstrap/dist/css/bootstrap-theme.min.css" rel="stylesheet" type="text/css">
+	<link href="bootstrap/dist/css/bootstrap.css" rel="stylesheet" type="text/css">
+	<link href="bootstrap/dist/css/bootstrap.css.map" rel="stylesheet" type="text/css">
+	<link href="bootstrap/dist/css/bootstrap.min.css" rel="stylesheet" type="text/css">
+
+	<link href="css/main.min.css" rel="stylesheet" type="text/css">
+	<link href="css/main.css" rel="stylesheet" type="text/css">
+
+	<script src="bootstrap/dist/js/bootstrap.js"></script>
+	<script src="bootstrap/dist/js/bootstrap.min.js"></script>
+	<script src="bootstrap/dist/js/npm.js"></script>
+
+	<script src="js/main.js"></script>
+</head>
+
+<body>
+	<div id="header">
+		<div id="header-logo"></div>
+		<div id="header-rest"></div>
+	</div>
+</body>
+</html>
